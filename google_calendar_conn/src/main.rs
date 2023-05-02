cfa655d84500215190dc7dd32ab331dabc6f01dd
wyyshnya <jefersonhrm1@gmail.com>
2023-05-02 20:49:14 +0300

    Google-calendar connector continue

--- google_calendar_conn/src/main.rs
+++ google_calendar_conn/src/main.rs
@@ -1,25 +1,34 @@
-use std::thread::scope;
-use chrono::Utc;
+use std::fmt::Debug;
+use chrono::{Duration, Utc};
 use google_calendar::Client;
 use google_calendar::types::{Event, EventDateTime};
+use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
 
 
 #[tokio::main]
 async fn main() {
-    // TODO нужно авторизоваться и получить Oauth token
+    // TODO сделать через фронт, как написано на листочке
     // Создание календаря
     let mut client = Client::new(
         String::from("1032867852781-vk3sth9p1c0fi2fvl0jddhu2c0uk6hj6.apps.googleusercontent.com"),
         String::from("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"),
-        String::from("https://oauth2.googleapis.com/token"),
-        String::from("ya29.a0AWY7CknVWDZfqYOWgYogGp9nrcJh-_7ezNBBY0OdZQxxDKt4EB4r4ucMEguR4Htrf6bMLOZrHyTLGMo5leAH8w8GlVLfIugYoLb7WsY3xPglr8IuMGLD1xXYOAHW_5BvcjP5wIIOQpCowlFLXeSKBz39759baCgYKAYISARASFQG1tDrpO6RbBigtXW-P8PmJZQFzWQ0163"),
-        String::from("1//0cRtbEVhW9YzgCgYIARAAGAwSNwF-L9Irmf3eh7nUOnhfCNPX4UgcIllRnIx1taVo2guWO1ZChzebRKN0tOxJXPFhUG82HM3Sg7Q"),
+        String::from("http://localhost"),
+        String::from(""),
+        String::from(""),
     );
 
     let user_consent_url = client.user_consent_url(&["https://www.googleapis.com/auth/calendar".to_string()]);
 
+    webbrowser::open(&*user_consent_url).unwrap();
+
+
+    let code = url::Url::parse(&*user_consent_url).unwrap().query_pairs().find(|(key, _)| key == "code").unwrap().1.to_string();
+    let state = url::Url::parse(&*user_consent_url).unwrap().query_pairs().find(|(key, _)| key == "state").unwrap().1.to_string();
+
+    let mut access_token = client.get_access_token(&*code, &*state).await.unwrap();
+
     let s = client.events().list_all("jefersonhrm1@gmail.com", "", 0, Default::default(), &[], "", &[], false, false, false, "", "", "", "").await;
-    println!("{:?}", s);
+    println!("{:?}", user_consent_url);
     let event = Event {
         anyone_can_add_self: true,
         attachments: vec![],
@@ -34,7 +43,7 @@ async fn main() {
         description: String::from("Description of my event"),
         end: Option::from(EventDateTime {
             date: None,
-            date_time: Option::from(chrono::DateTime::<Utc>::from_local(chrono::Local::now().naive_local(), Utc)),
+            date_time: Option::from(chrono::DateTime::<Utc>::from_local(chrono::Local::now().naive_local() + Duration::hours(1), Utc)),
             time_zone: "".to_string()
         }),
         end_time_unspecified: false,
@@ -69,30 +78,25 @@ async fn main() {
         status: "".to_string(),
         visibility: "".to_string(),
     };
-    // let p = client.events().insert("jefersonhrm1@gmail.com",
-    //              0,
-    //              0,
-    //              false,
-    //              Default::default(),
-    //              false,
-    //              &event).await;
-    let p = client.events().insert("2c3b15364f6c792022d82abd9329dbde69a04092187e9d05f4655f5170efb872@group.calendar.google.com",
+
+    let p = client.events().insert("jefersonhrm1@gmail.com",
                                    0,
                                    0,
                                    false,
                                    Default::default(),
                                    false,
-                                   &Default::default()).await;
-    // let p = client.acl().insert("2c3b15364f6c792022d82abd9329dbde69a04092187e9d05f4655f5170efb872@group.calendar.google.com", false,
-    //                             &google_calendar::types::AclRule {
-    //                                 etag: "".to_string(),
-    //                                 id: "".to_string(),
-    //                                 kind: "calendar#aclRule".to_string(),
-    //                                 role: "writer".to_string(),
-    //                                 scope: Option::from(google_calendar::types::Scope {
-    //                                     type_: "user".to_string(),
-    //                                     value: "jefersonhrm1@gmail.com".to_string(),
-    //                                 }),
-    //                             }).await;
+                                   &event).await;
+    // // let p = client.acl().insert("2c3b15364f6c792022d82abd9329dbde69a04092187e9d05f4655f5170efb872@group.calendar.google.com", false,
+    // //                             &google_calendar::types::AclRule {
+    // //                                 etag: "".to_string(),
+    // //                                 id: "".to_string(),
+    // //                                 kind: "calendar#aclRule".to_string(),
+    // //                                 role: "writer".to_string(),
+    // //                                 scope: Option::from(google_calendar::types::Scope {
+    // //                                     type_: "user".to_string(),
+    // //                                     value: "jefersonhrm1@gmail.com".to_string(),
+    // //                                 }),
+    // //                             }).await;
     println!("{:?}", p);
 }
+
